835806da64d1d35c9777eaaf7f3fe1f9064c290c
Dottyboi <Fabian.pizarro.04@gmail.com>
2022-02-13 18:17:09 +0100

    json
    added json functions and remade structure of package

--- src/mypack/funcs.py
+++ src/mypack/funcs.py
@@ -1,18 +1,17 @@
-import os
-import pickle
-import json
+import os, sys, pickle, json
+from collections import namedtuple
 
 # = Clear
 
 def clear():  
-    """Clears os"""
+    """Clears terminal"""
     os.system("cls" if os.name == "nt" else "clear")
 
 # = pickle lister
 
 def depickler(filename):
     """writes object to list from file"""
-    with open(filename, "rb") as pickle_file:
+    with open(os.path.join(sys.path[0], f"{filename}.pickle"), "rb") as pickle_file:
         objects = []
         while True:
             try:
@@ -25,7 +24,7 @@ def depickler(filename):
 
 def file_lister(file):
     """takes file as string input, a variable for the file (it can be whatever as long as it is changeable) and an empty list as input"""
-    with open(file, "r") as file_variable:
+    with open(os.path.join(sys.path[0], file), "r") as file_variable:
         file_list = file_variable.readlines()  # ¤ writes file content to list
         for i in range(len(file_list)):  # ¤ strips the content of the list
             file_list[i] = file_list[i].strip()
@@ -47,7 +46,7 @@ def intput(printable, value_error_text = "You didnt write a number."):
 
 def picklify(obj_list, filename):
     """Saves object to file"""
-    with open(filename, "wb") as file:  # ¤ Overwrites any existing file.
+    with open(os.path.join(sys.path[0], f"{filename}.pickle"), "wb") as file:  # ¤ Overwrites any existing file.
         for i in range(len(obj_list)):
             pickle.dump(obj_list[i], file, pickle.HIGHEST_PROTOCOL)
         file.close()
@@ -72,24 +71,15 @@ def strintput(printable, value_error_text = "You didnt write a number."):
 
 # = read from json file
 
-def json_classreader(classname, filename):
-    """reads from json file and returns class attributes"""
-    with open(filename + ".json", "r") as read_file:
-        data = json.load(read_file)
+def json_reader(filename):
+    """reads from json file and returns dict"""
+    with open(os.path.join(sys.path[0], f'{filename}.json'), "r") as read_file:
+        return json.load(read_file)
 
-    try:    
-        return [
-            classname(value for _, value in data.items())
-            for _ in range(len(data))
-        ]
-    except TypeError:
-        print("Unable to initiate objects")
-        return []
 
 # = write to json file
 
-def json_classwriter(data, filename):
-    """writes class attributes to json"""
-    with open(filename + ".json", "w") as write_file:
+def json_writer(data, filename):
+    """writes dict to json"""
+    with open(os.path.join(sys.path[0], f'{filename}.json'), "w") as write_file:
         json.dump(data.__dict__.items(), write_file)
-
